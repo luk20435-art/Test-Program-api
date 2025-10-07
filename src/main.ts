e6c9d8dcbe3e2e5dcbd6f577e535644a5c38694d
Ruslee Seebu <ruslee.seebu@dga.or.th>
2025-10-07 22:30:40 +0700

    edit main

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   console.log(`Application is running on: ${process.env.PORT}`);
   await app.listen(process.env.PORT ?? 3000);
 }
